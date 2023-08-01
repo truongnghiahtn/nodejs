7beec006480d41798db446123d994406098667bb
Trương Đức Nghĩa <nghia_td@dev.anybim.vn>
2023-08-01 20:45:23 +0700

    scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// config file static
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -15,12 +19,14 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+
+//router
 app.get('/', (req, res) => {
     res.render('home');
 })
-
 app.get('/news', (req, res) => {
     res.render('news');
 })
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
