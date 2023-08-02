6f8a2a3918659db677dbbe0f10269be300a3638c
Trương Đức Nghĩa <nghia_td@dev.anybim.vn>
2023-08-02 08:50:18 +0700

    update route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 // config file static
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,12 +23,7 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 
 //router
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+route(app);
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ src/routes/site.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const newsController = require('../app/controllers/SiteController');
+
+router.use('/search', newsController.search);
+router.use('/', newsController.home);
+
+module.exports = router;
+
