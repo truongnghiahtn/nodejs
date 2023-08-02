c45bae70316b7f33f7de80d956086c451a53d508
Trương Đức Nghĩa <nghia_td@dev.anybim.vn>
2023-08-02 13:02:49 +0700

    format

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
 // config file static
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Template engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
-
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 //router
 route(app);
 
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const newsController = require('../app/controllers/SiteController');
+const newsController = require("../app/controllers/SiteController");
 
-router.use('/search', newsController.search);
-router.use('/', newsController.home);
+router.use("/search", newsController.search);
+router.use("/", newsController.home);
 
 module.exports = router;
-
